0f03ef1a83d805ca3a6c6376b83b8b6f0962031f
Tuffk <tuffkmulhall@gmail.com>
2017-06-13 20:08:36 +0300

    models

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,5 @@
 class House < ApplicationRecord
+  belongs_to :user
+
+  validates :address, presece: true, length: { minimum: 8 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :houses
+  validates :name, :password, presece: true, length: { in: 3..50 }
 end
