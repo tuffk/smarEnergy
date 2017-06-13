31643a25fc59e28080ccca3597b7d8a3e40ff1ea
Tuffk <tuffkmulhall@gmail.com>
2017-06-13 19:55:31 +0300

    first models

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module PredixRailsStarterApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     # Configure generators
     config.generators do |g|
