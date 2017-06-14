0f4225a2d66de90f3fd0ff042674e7583068af69
Lainie Wright <lainiewright@gmail.com>
2017-06-14 15:23:27 +0300

    working

--- ui/widgets/energymonitor/component.js
+++ ui/widgets/energymonitor/component.js
@@ -2,7 +2,13 @@ var config = require('../../config');
 angular.module("energymonitor")
     .component("energymonitorComp", {
         templateUrl: '/widgets/energymonitor/template.html',
-        controller: function ($scope, $http, $window) {
-
+        controller: function ($scope, $http, $interval) {
+        	var user_id = 1;
+        	var url = config.base_url + "/capture/" + user_id;
+        	$http.get(url, []).then(function(response) {
+        			if (response.status == 200) {
+        				console.log(response);
+        			}
+        		});
         }
     });
