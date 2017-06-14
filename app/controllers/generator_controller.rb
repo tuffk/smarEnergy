3b48df7e72e9f63b54f60e3777aa9973ae673f11
Tuffk <tuffkmulhall@gmail.com>
2017-06-14 15:47:44 +0300

    generator adjusting

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -5,10 +5,10 @@ class GeneratorController < ApplicationController
     user = User.find(params[:uid]) unless params[:uid].nil?
     house = House.find(params[:hid]) unless params[:hid].nil?
     device = Device.find(params[:did]) unless params[:did].nil?
-    for i in 1..10 do # rubocop:disable For
-      Capture.create(device: device, start: rand(1..100).minutes.ago,
-                     stop: rand(1..100).minutes.from_now, amount: rand(1..500))
-      sleep(1)
+    for i in 1..50 do # rubocop:disable For
+      Capture.create(device: device, start: rand(1..1000).seconds.ago,
+                     stop: rand(1..1000).seconds.from_now, amount: rand(1..500))
+      # sleep(1)
     end
   end
 end
