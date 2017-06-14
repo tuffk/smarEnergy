ba21ef4319e3892485307812f6878aff59b0e1fa
Tuffk <tuffkmulhall@gmail.com>
2017-06-14 11:18:47 +0300

    Serializer for house

--- app/serializers/house_serializer.rb
+++ app/serializers/house_serializer.rb
@@ -1,8 +1,20 @@
 class HouseSerializer < ActiveModel::Serializer
   attributes :id, :name, :area, :rooms, :address
-  attribute :energy_percentage
+  attributes :energy_total, :water_total, :p_percentage, :w_percentage
 
-  def energy_percentage
+  def energy_total
     object.energy_total
+  end
+
+  def water_total
+    object.water_total
+  end
+
+  def p_percentage
+    object.energy_p
+  end
+
+  def w_percentage
+    object.water_p
   end
 end
